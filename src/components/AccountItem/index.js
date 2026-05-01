c12dd77b722a590e1d5ca3d465c858b5e4c7c8ef
TuanNH <tuannguyenhuy@vccorp.vn>
2026-05-01 13:52:23 +0700

    Xây dựng Logic phần Header #1

--- src/components/AccountItem/index.js
+++ src/components/AccountItem/index.js
@@ -1,6 +1,7 @@
-import classNames from 'classnames/bind';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faCheckCircle } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import classNames from 'classnames/bind';
+import Image from '~/components/Image';
 import styles from './AccountItem.module.scss';
 
 const cx = classNames.bind(styles);
@@ -8,13 +9,17 @@ const cx = classNames.bind(styles);
 function AccountItem() {
   return (
     <div className={cx('wrapper')}>
-      <img className={cx('avatar')} src="https://www.w3schools.com/tags/img_girl.jpg" alt="my-girl" />
+      <Image
+        className={cx('avatar')}
+        src="https://p16-sign-va.tiktokcdn.com/tos-useast2a-avt-0068-giso/ea0854578085ab26effc2c7b8cefa270~c5_300x300.webp?x-expires=1651489200&x-signature=Xp8iH14MDegegBpFEg%2Fg5rv6Mrw%3D"
+        alt="Hoaa"
+      />
       <div className={cx('info')}>
         <h4 className={cx('name')}>
-          <span>Nguyễn Văn A</span>
+          <span>Nguyen Van A</span>
           <FontAwesomeIcon className={cx('check')} icon={faCheckCircle} />
         </h4>
-        <span className={cx('username')}>nguyen.van.a</span>
+        <span className={cx('username')}>nguyenvana</span>
       </div>
     </div>
   );
